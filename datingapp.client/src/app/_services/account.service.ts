0c55a6861576d0a7ef8970ecd4fd4d45647f41fd
Matheus <matheusfarnetani@gmail.com>
2025-01-24 14:33:16 -0300

    4º commit

---
+++ datingapp.client/src/app/_services/account.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { IUser } from '../_models/IUser';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private http = inject(HttpClient);
+  baseUrl = 'https://localhost:7003/api/';
+  currentUser = signal<IUser | null>(null);
+
+  login(model: any) {
+    return this.http.post<IUser>(this.baseUrl + 'account/login', model).pipe(
+      map(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+
+  register(model: any) {
+    return this.http.post<IUser>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+        return user;
+      })
+    );
+  }
+}
