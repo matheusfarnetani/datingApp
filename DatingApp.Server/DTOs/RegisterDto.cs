b91a40a3219eab715224d6f6fdf0f5e397c5b917
Matheus <matheusfarnetani@gmail.com>
2025-01-27 18:00:35 -0300

    6º commit

--- DatingApp.Server/DTOs/RegisterDto.cs
+++ DatingApp.Server/DTOs/RegisterDto.cs
@@ -8,9 +8,10 @@ namespace DatingApp.Server.DTOs
     {
         [Required]
         [MaxLength(100)]
-        public required string Username { get; set; }
-        
+        public string Username { get; set; } = string.Empty;
+
         [Required]
-        public required string Password { get; set; }
+        [StringLength(8, MinimumLength = 4)]
+        public string Password { get; set; } = string.Empty;
     }
 }
--- DatingApp.Server/Program.cs
+++ DatingApp.Server/Program.cs
@@ -1,4 +1,5 @@
 using DatingApp.Server.Extensions;
+using DatingApp.Server.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,9 @@ builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
+
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // CORS
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
